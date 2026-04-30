ee1d1241f26fd6941262001342513b2bba914e2a
jacoacoacob <jacoacoacob@protonmail.com>
2026-04-29 21:48:25 -0500

    add bookmark utils

--- rl_shared/src/bookmark.rs
+++ rl_shared/src/bookmark.rs
@@ -2,6 +2,29 @@ use std::fmt::Display;
 
 use chrono::{DateTime, Utc};
 
+pub fn is_bookmark_url_field(value: &str) -> bool {
+    let value = value.trim();
+
+    value.starts_with("https://")
+        || value.starts_with("http://")
+        || value.starts_with("file://")
+        // :nogo: is for bookmarks that don't need to link to an external file or web page
+        // (i.e. ones you're saving during development and testing)
+        || value.starts_with(":nogo:")
+}
+
+pub fn extract_url_from_autocomplete_selection(choice: &str) -> Option<String> {
+    for line in choice.split('\n') {
+        let line = line.trim();
+
+        if is_bookmark_url_field(line) {
+            return Some(line.to_string());
+        }
+    }
+
+    None
+}
+
 #[derive(Debug, Clone)]
 pub struct Bookmark {
     pub url: String,
@@ -29,6 +52,10 @@ impl Bookmark {
     pub fn builder() -> BookmarkBuilder {
         BookmarkBuilder::default()
     }
+
+    pub fn is_nogo(&self) -> bool {
+        return self.url.starts_with(":nogo:");
+    }
 }
 
 #[derive(Debug, Default)]
@@ -40,7 +67,6 @@ pub struct BookmarkBuilder {
     updated: Option<DateTime<Utc>>,
     visited: Option<DateTime<Utc>>,
 }
-
 
 impl BookmarkBuilder {
     pub fn url(mut self, url: &str) -> Self {
@@ -88,28 +114,29 @@ impl BookmarkBuilder {
 impl Display for Bookmark {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         if f.alternate() {
+            let name = self.name.to_string();
+            let tags = self.tags.join(",");
+            let url = self.url.to_string();
+            let created = self.created;
+            let updated = self.updated;
+            let visited = self.visited;
+
             write!(
                 f,
                 "
-  NAME    :    {}
-  TAGS    :    {}
-  URL     :    {}
-  CREATED :    {}
-  UPDATED :    {}
-  VISITED :    {}
-",
-            self.name,
-            self.tags.join(", "),
-            self.url,
-            self.created,
-            self.updated,
-            self.visited
+   name    | {name}
+   tags    | {tags}
+   url     | {url}
+   created | {created}
+   updated | {updated}
+   visited | {visited}
+"
             )
         } else {
             let mut display = String::from("");
 
             display.push_str(&format!("{}\n", self.name));
-            
+
             if self.tags.len() > 0 {
                 display.push_str(&format!("  {}\n", self.tags.join(", ")));
             }
