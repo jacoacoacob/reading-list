bf5540f752d32d24185326546e18dd14f28d8f96
jacoacoacob <jacoacoacob@protonmail.com>
2026-04-29 21:52:57 -0500

    update lib exports

--- rl_shared/src/lib.rs
+++ rl_shared/src/lib.rs
@@ -1,7 +1,10 @@
 mod bookmark;
 mod config;
+mod context;
 mod database;
 
-pub use bookmark::Bookmark;
+pub use bookmark::{Bookmark, extract_url_from_autocomplete_selection, is_bookmark_url_field};
 pub use config::Config;
+pub use context::Context;
 pub use database::Database;
+pub use chrono::Utc;
